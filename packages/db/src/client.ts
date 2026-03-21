4a8f7a8c8b5c2882125e04fc1d42df110437ae5f
Alec Pullen <alecpullen24@gmail.com>
2026-03-22 02:04:00 +1100

    fix pg interop

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,10 +1,11 @@
 import 'dotenv/config'
-import { Pool } from 'pg'
+import pg from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
 
-const connectionString = process.env.DATABASE_URL!
+const { Pool } = pg
 
+const connectionString = process.env.DATABASE_URL!
 const pool = new Pool({ connectionString })
 const adapter = new PrismaPg(pool)
 
